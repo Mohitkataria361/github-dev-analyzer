27f2fe7cab05ea23ba56eb8f80aad98159815fd5
Mohit Kataria <reenakataria75@gmail.com>
2026-07-08 10:08:51 +0530

    feat: build responsive navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
      <div>
-      <h1 className="text-xl font-bold text-gray-500 pl-2 pt-3">Analyze any GitHub Profile</h1>
+       <main className="mx-auto max-w-7xl px-6 py-16">
+      <h1 className="text-5xl font-bold tracking-tight">
+        Analyze Any GitHub Profile
+      </h1>
+
+      <p className="mt-4 max-w-2xl text-lg text-gray-600">
+        Get detailed insights into repositories, programming languages,
+        contribution statistics, stars, forks, and much more.
+      </p>
+    </main>
     </div>
   );
 }
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,31 +1,63 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+"use client";
+
+import Link from "next/link";
+import { GitBranch } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const Navbar = () => {
   return (
-    <div className='span-2 w-full bg-gray-700 text-white p-4'>
-      <ul className='flex justify-evenly'><li>Home
-      </li>
-      <li>
-        {/* <input className="bg-gray-800 p-2 w-4xl test-white-700 placeholder:text-gray-500  border-gray-300 focus:outline-none focus:ring-2 focus:gray-500" type="text" placeholder='Search github username'/> */}
-        <SearchBar/>
-      </li>
-      <li></li>
-      <li>
-        Favorites
-      </li>
-      <li>
-        Compare
-      </li>
-      <li>
-        About
-      </li>
-      <li>
-        Profile
-      </li>
-      </ul>
-    </div>
-  )
-}
+    <nav className="sticky top-0 z-50 w-full border-b bg-gray-400 backdrop-blur-lg">
+      <div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6">
+        {/* Left */}
+        <div className="flex items-center gap-2">
+          <GitBranch className="h-7 w-7" />
+          <Link
+            href="/"
+            className="text-xl font-bold tracking-tight hover:text-blue-600 transition-colors"
+          >
+            RepoLens
+          </Link>
+        </div>
 
-export default Navbar
+        {/* Center */}
+        <div className="hidden w-full max-w-md px-6  md:block">
+          <Input
+            type="text"
+            className="w-full rounded-md border border-gray-950 bg-white px-4 py-2 text-sm text-gray-700 placeholder-gray-400 focus:border-gray-600 focus:ring focus:ring-blue-200"
+            placeholder="Search GitHub Username..."
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center gap-6 font-medium">
+          <Link href="/" className="hover:text-blue-600 transition-colors">
+            Home
+          </Link>
+
+          <Link
+            href="/compare"
+            className="hover:text-blue-600 transition-colors"
+          >
+            Compare
+          </Link>
+
+          <Link
+            href="/about"
+            className="hover:text-blue-600 transition-colors"
+          >
+            About
+          </Link>
+
+          <Link
+            href="/login"
+            className="rounded-md bg-black px-4 py-2 text-white hover:bg-neutral-800 transition-colors"
+          >
+            Login
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
